962406cea5f6dfa095a04d942d2c6a2c9c2bfa2c
geoffsee <gseemueller@gmail.com>
2019-08-13 22:02:53 -0400

    Add files via upload

---
+++ splice script.js
@@ -0,0 +1,6 @@
+// Get all sounds in List
+//API Structure is 
+
+function getAllSouundsInList() {
+for (i=0; i < document.getElementsByClassName("sounds-list-item__checkbox").length; i++) document.getElementsByClassName("sounds-list-item__checkbox")[i].click();
+}
